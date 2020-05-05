82900ce0822c61d1f24ebd19c1d13cccdeb61ff5
hydrangea-io <hydrangea.io.inc@gmail.com>
2020-05-05 16:04:13 -0400

    Add base project files.

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Jie Lin on 5/5/20.
+//  Copyright © 2020 Jie Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
